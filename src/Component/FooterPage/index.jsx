4900e88358bba6bdf0cdc1b7e4fd432ac3a3714d
massimo olivo <massimo.olivo@gmail.com>
2019-05-09 01:42:52 +0200

    retouche

--- src/Component/FooterPage/index.jsx
+++ src/Component/FooterPage/index.jsx
@@ -1,26 +1,40 @@
 import React from "react";
-import { Navbar } from "react-bootstrap";
+import facebook from './../../assets/icons/facebook.svg';
+import twitter from './../../assets/icons/twitter.svg';
+import youtube from './../../assets/icons/youtube.svg';
+import instagram from './../../assets/icons/instagram.svg';
+import github from './../../assets/icons/github.svg';
+import linkedin from './../../assets/icons/linkedin.svg';
 import './index.css'
 
 
 
 const FooterPage = () => {
   return (
-    <>
   
-  <Navbar bg="dark" variant="dark">
-    <Navbar.Brand href="https://wildcodeschool.be/">
-      <img
-        alt=""
-        src="/logo.svg"
-        width="30"
-        height="30"
-        className="d-inline-block align-top"
-      />
-      {' wild code scool'}
-    </Navbar.Brand>
-  </Navbar>
-</>
+  <footer className="footer">
+    <ul className="fond">
+      <li className="icone-footer">
+        <a href="https://www.facebook.com/wildcodeschool"><img src={facebook} alt="facebook" /></a>
+      </li>
+      <li className="icone-footer">
+        <a href="https://twitter.com/wildcodeschool"><img src={twitter} alt="twitter" /></a>
+      </li>
+      <li className="icone-footer">
+        <a href="https://www.youtube.com"><img src={youtube} alt="youtube" /></a>
+      </li>
+      <li className="icone-footer">
+        <a href="https://www.instagram.com/wildcodeschool/"><img src={instagram} alt="instagram" /></a>
+      </li>
+      <li className="icone-footer">
+        <a href="https://github.com/WildCodeSchool/"><img src={github} alt="github" /></a>
+      </li>
+      <li className="icone-footer">
+        <a href="https://www.linkedin.com/school/wild-codes-chool?pathWildcard=10387519/"><img src={linkedin} alt="linkedin" /></a>
+      </li>
+    </ul>
+  </footer>
+
   );
 }
 
