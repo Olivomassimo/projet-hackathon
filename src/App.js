b2ffda8a715761a2cfaf7c3d49dae6cb23806d31
caval1289 <46388282+caval1289@users.noreply.github.com>
2019-05-08 23:13:27 +0200

    Revert "boutton1"

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import{Button } from 'react-bootstrap'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css'
 import Header from './component/Header/index'
-import ChoixBoutton from './component/Bouton/index'
-
 
 function App() {
   return (
   <div>
-    <ChoixBoutton/>
+    <Button variant="primary">Primary</Button>
+    <Header/>
 
   </div>
   
