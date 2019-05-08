9b9fbeabf376389591a2e268e60fa2cf537e1d80
massimo olivo <massimo.olivo@gmail.com>
2019-05-08 15:45:48 +0200

    init repo

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react' ;
+import{Button } from 'react-bootstrap'
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import Header from './component/Header/index'
+
+function App() {
+  return (
+  <div>
+    <Button variant="primary">Primary</Button>
+    <Header/>
+
+  </div>
+  
+  );
+}
+
+export default App;
