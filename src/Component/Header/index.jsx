8648ec68c7f352de3ab065f6e6d7fbea6636fe5f
massimo olivo <massimo.olivo@gmail.com>
2019-05-09 16:32:07 +0200

    merche

--- src/Component/Header/index.jsx
+++ src/Component/Header/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
-import wild1 from './../../image/wild1.jpg';
-import wild2 from './../../image/wild1.jpg';
-import wild3 from './../../image/wild1.jpg';
+import wildCode3 from './../../image/wildCode3.jpg';
+import wildCode2 from './../../image/wildCode2.jpg';
+import wildCode from './../../image/wildCode.jpg';
 import './index.css'
 
 
@@ -15,36 +15,33 @@ const Header = () => {
             <Carousel.Item>
                 <img
                     className="d-block w-100"
-                    src={wild1}
+                    src={wildCode3}
                     alt="First slide"
                 />
                 <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img
                     className="d-block w-100"
-                    src={wild2}
+                    src={wildCode2}
                     alt="Third slide"
                 />
 
                 <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img
                     className="d-block w-100"
-                    src={wild3}
+                    src={wildCode}
                     alt="Third slide"
                 />
 
                 <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                   
                 </Carousel.Caption>
             </Carousel.Item>
         </Carousel>
