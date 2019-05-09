3030820e48b72c5f42795d11d0356a01da99a8ca
massimo olivo <massimo.olivo@gmail.com>
2019-05-09 10:15:56 +0200

    fin pour alex

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react' ;
-import{Button } from 'react-bootstrap'
+import Carts from './component/Carts/index'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css'
-import Header from './component/Header/index'
+
 
 function App() {
   return (
   <div>
-    <Button variant="primary">Primary</Button>
-    <Header/>
+    <Carts />
 
   </div>
   
---
+++ src/component/Carts/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './index.css';
+
+
+
+
+const Carts = () => {
+    return (
+        <div>
+            <ul className="card-list">
+                <li className="card">
+                    <div className="image-container">
+                        <img className="" 
+                        src={""}
+                        alt="eleve">
+                        </img>
+                    </div>
+                    <div className="content">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <button>selec</button>
+                    </div>
+                </li>
+               
+            </ul>
+        </div>
+    );
+};
+
+export default Carts;
