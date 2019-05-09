3f5f4b2bab462eae45085c4b1f40e89ef2291b2e
massimo olivo <massimo.olivo@gmail.com>
2019-05-09 10:26:42 +0200

    fin pour alex1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react' ;
-import Carts from './component/Carts/index'
+import Carts from './component/Carts/index';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.css'
 
 
 function App() {
