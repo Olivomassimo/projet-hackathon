0837c3b9c76deff9d0200b82a1a78543aceea9c4
massimo olivo <massimo.olivo@gmail.com>
2019-05-09 12:24:57 +0200

    cest cadeau

--- src/Component/FooterPage/index.jsx
+++ src/Component/FooterPage/index.jsx
@@ -5,6 +5,7 @@ import youtube from './../../assets/icons/youtube.svg';
 import instagram from './../../assets/icons/instagram.svg';
 import github from './../../assets/icons/github.svg';
 import linkedin from './../../assets/icons/linkedin.svg';
+import wild from './../../assets/icons/wild.jpg'
 import './index.css'
 
 
@@ -15,22 +16,25 @@ const FooterPage = () => {
   <footer className="footer">
     <ul className="fond">
       <li className="icone-footer">
-        <a href="https://www.facebook.com/wildcodeschool"><img src={facebook} alt="facebook" /></a>
+        <a href="https://www.facebook.com/wildcodeschoolbelgium/"><img src={facebook} alt="facebook" /></a>
       </li>
       <li className="icone-footer">
-        <a href="https://twitter.com/wildcodeschool"><img src={twitter} alt="twitter" /></a>
+        <a href="https://twitter.com/WildSchool_Bxl"><img src={twitter} alt="twitter" /></a>
       </li>
       <li className="icone-footer">
         <a href="https://www.youtube.com"><img src={youtube} alt="youtube" /></a>
       </li>
       <li className="icone-footer">
-        <a href="https://www.instagram.com/wildcodeschool/"><img src={instagram} alt="instagram" /></a>
+        <a href="https://www.instagram.com/wildcodeschoolbelgium/?hl=fr"><img src={instagram} alt="instagram" /></a>
       </li>
       <li className="icone-footer">
         <a href="https://github.com/WildCodeSchool/"><img src={github} alt="github" /></a>
       </li>
       <li className="icone-footer">
-        <a href="https://www.linkedin.com/school/wild-codes-chool?pathWildcard=10387519/"><img src={linkedin} alt="linkedin" /></a>
+        <a href="https://www.linkedin.com/school/wild-codes-chool/"><img src={linkedin} alt="linkedin" /></a>
+      </li>
+      <li  className="icone-footer">
+        <a className="wild" href="https://wildcodeschool.be/brussels/"><img class="image" src={wild} alt="wild" /></a>
       </li>
     </ul>
   </footer>
