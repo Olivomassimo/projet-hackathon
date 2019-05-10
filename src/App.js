a83b4c5f41f469772791cacf4e04b12b4ef6d1b8
Alessandro Cavaleri <caval1289@gmail.com>
2019-05-10 04:31:48 +0200

    fin

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import React from 'react' ;
+import React from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Header from './Component/Header/index'
 import 'bootstrap/dist/css/bootstrap.css'
 import NavWild from './component/Nav/index'
 import CarteEleve from './component/Carteeleve/index'
+import Team from './component/Team/index';
+import DisplayEleve from './component/Apicarte';
 import FooterPage from './Component/FooterPage/index';
 
-function App () {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedStudent: []
+    }
+  }
+
+  selectStudent = (elem) => {
+    const currentSelection = this.state.selectedStudent;
+    currentSelection.push(elem)
+
+    this.setState({
+      selectStudent: currentSelection
+    })
+  }
+  render() {
+    const { selectedStudent } = this.state;
     return (
       <div className='App'>
-      <NavWild />
-      <CarteEleve />
-      <Switch>
-        <Route exact path='/' component={Header} />
-        <Route path='/time'  component={Header} />
-        <Route path='/sine-up' component={Header.NavWild} />
-      </Switch>
-      <FooterPage />
+        <NavWild counter={selectedStudent.length} />
+        <Switch>
+          <Route exact path='/' component={Header} />
+          <Route path='/student' render={() => <CarteEleve selectStudent={this.selectStudent} />} />
+          <Route path='/team' render={() => <Team data={selectedStudent} />} />
+          <Route path='/sine-up' component={Header.NavWild} />
+          <Route path='/DisplayEleve/:mas' component={ DisplayEleve }/>
+        </Switch>
+        <FooterPage/>
       </div>
     );
   }
+}
 
 export default App;
